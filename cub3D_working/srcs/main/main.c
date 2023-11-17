e5f9b3108f3cf019ab754bf3317990c90d8b76f6
Drew Pentland <drewtpentland@gmail.com>
2023-11-17 11:15:31 +0100

    Fixing norm
    running initial github action checks to make sure everything is working

--- cub3D_working/srcs/main/main.c
+++ cub3D_working/srcs/main/main.c
@@ -6,14 +6,14 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 10:46:20 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/17 11:12:12 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/11/17 11:14:48 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 #include <stdio.h>
 
-int main (void)
+int	main(void)
 {
 	printf("Hello cub3D: %d\n", WORKING);
 	printf("Checking github status\n");
