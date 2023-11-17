bcc6d9ed2253bd62ea4bfc0826733346900fde0e
Drew Pentland <drewtpentland@gmail.com>
2023-11-17 13:45:35 +0100

    Added my libft and minilibx from the school
    Added my libft (a copy from my fdf project)
    Also added the minilibx from the school
    Heavy editing on the makefile to link everything.
    No norm update yet

--- cub3D_working/libft/srcs/ft_memchr.c
+++ cub3D_working/libft/srcs/ft_memchr.c
@@ -1,32 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/17 10:46:20 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/17 13:41:08 by dpentlan         ###   ########.fr       */
+/*   Created: 2022/11/14 10:58:12 by dpentlan          #+#    #+#             */
+/*   Updated: 2022/11/14 10:58:16 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3D.h"
 #include "libft.h"
-#include <mlx.h>
-#include <stdio.h>
 
-int	main(void)
+/*  *** ft_memchr (42 memory character) ***
+ *
+ *  Takes a pointer 's', an integer 'c' and size_t 'n'.
+ *  Checks 'n' number of bytes for character 'c'.
+ *  Returns address of first found byte that matches 'c'.
+ *  If none is found, returns 0.
+ */
+
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	void		*mlx_ptr;
-	void		*win_ptr;
-	void		*img_ptr;
+	size_t	i;
 
-	mlx_ptr = mlx_init();
-	printf("Hello cub3D: %d\n", WORKING);
-	printf("Checking github status\n");
-	printf("ft_strlen: %ld\n", ft_strlen("hi"));
-	win_ptr = mlx_new_window(mlx_ptr, 100, 100, "whats up");
-	img_ptr = mlx_new_image(mlx_ptr, 100, 100);
-	mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 0, 0);
+	i = 0;
+	while (i < n)
+	{
+		if (*((unsigned char *)s + i) == (unsigned char)c)
+			return (((unsigned char *)s + i));
+		i++;
+	}
 	return (0);
 }
--- cub3D_working/libft/srcs/ft_strchr.c
+++ cub3D_working/libft/srcs/ft_strchr.c
@@ -1,32 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/17 10:46:20 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/17 13:41:08 by dpentlan         ###   ########.fr       */
+/*   Created: 2022/11/11 15:07:07 by dpentlan          #+#    #+#             */
+/*   Updated: 2022/11/11 15:07:10 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3D.h"
 #include "libft.h"
-#include <mlx.h>
-#include <stdio.h>
 
-int	main(void)
+/*  *** ft_strchr (42 string character) ***
+ *
+ *  Takes a string pointer 's' and a character 'c'.
+ *  Returns the address of the first instance of character 'c'.
+ */
+
+char	*ft_strchr(const char *s, int c)
 {
-	void		*mlx_ptr;
-	void		*win_ptr;
-	void		*img_ptr;
+	int	i;
 
-	mlx_ptr = mlx_init();
-	printf("Hello cub3D: %d\n", WORKING);
-	printf("Checking github status\n");
-	printf("ft_strlen: %ld\n", ft_strlen("hi"));
-	win_ptr = mlx_new_window(mlx_ptr, 100, 100, "whats up");
-	img_ptr = mlx_new_image(mlx_ptr, 100, 100);
-	mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 0, 0);
+	i = 0;
+	if (s == 0)
+		return (0);
+	while (s[i] != 0)
+	{
+		if (s[i] == (unsigned char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == (unsigned char)c)
+		return ((char *)&s[i]);
 	return (0);
 }
