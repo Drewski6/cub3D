835c2beb58e817d4e26e1d48d9a3b6c85f4cbb05
Drew Pentland <drewtpentland@gmail.com>
2023-11-18 13:29:46 +0100

    update to debug print due to changing var names

--- srcs/debug/map_debug.c
+++ srcs/debug/map_debug.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/18 12:51:14 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/18 13:19:13 by dpentlan         ###   ########.fr       */
+/*   created: 2023/11/18 12:51:14 by dpentlan          #+#    #+#             */
+/*   Updated: 2023/11/18 13:29:33 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,26 +33,26 @@ void	debug_print_table(char **table)
 
 void	debug_print_map_data(t_map_data *map_data)
 {
-	if (map_data->NO)
-		ft_printf("NO: %s\n", map_data->NO);
+	if (map_data->no)
+		ft_printf("no: %s\n", map_data->no);
 	else
-		ft_printf("NO: (NULL)\n");
-	if (map_data->SO)
-		ft_printf("SO: %s\n", map_data->SO);
+		ft_printf("no: (NULL)\n");
+	if (map_data->so)
+		ft_printf("so: %s\n", map_data->so);
 	else
-		ft_printf("SO: (NULL)\n");
-	if (map_data->EA)
-		ft_printf("EA: %s\n", map_data->EA);
+		ft_printf("so: (NULL)\n");
+	if (map_data->ea)
+		ft_printf("ea: %s\n", map_data->ea);
 	else
-		ft_printf("EA: (NULL)\n");
-	if (map_data->WE)
-		ft_printf("WE: %s\n", map_data->WE);
+		ft_printf("ea: (NULL)\n");
+	if (map_data->we)
+		ft_printf("we: %s\n", map_data->we);
 	else
-		ft_printf("WE: (NULL)\n");
-	ft_printf("F: R(%d), G(%d), B(%d)\n",
-		map_data->F.red, map_data->F.green, map_data->F.blue);
-	ft_printf("C: R(%d), G(%d), B(%d)\n",
-		map_data->C.red, map_data->C.green, map_data->C.blue);
+		ft_printf("we: (NULL)\n");
+	ft_printf("f: R(%d), G(%d), B(%d)\n",
+		map_data->f.red, map_data->f.green, map_data->f.blue);
+	ft_printf("c: R(%d), G(%d), B(%d)\n",
+		map_data->c.red, map_data->c.green, map_data->c.blue);
 	ft_printf("map: ");
 	if (map_data->map)
 		debug_print_table(map_data->map);
