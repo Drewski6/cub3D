a5b5c82aed33b253f0469b5d9cf0c0f1565fb32e
Drew Pentland <drewtpentland@gmail.com>
2023-11-18 17:11:01 +0100

    Reading in no, so, ea, and we from map
    Re-organized a little bit. Added some clean up functions and improved my
    libft as well.
    Most significant change is in map_parsing.c we now have  copy of the map
    contents and paths in our struct which is also freed when the program
    exits.

--- srcs/debug/map_debug.c
+++ srcs/debug/map_debug.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 13:41:57 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/18 13:54:08 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/11/18 15:30:44 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ void	debug_print_map_data(t_map_data *map_data)
 		map_data->c.red, map_data->c.green, map_data->c.blue);
 	ft_printf("map: ");
 	if (map_data->map)
-		ft_print_table(map_data->map);
+		ft_print_table(map_data->map, 0);
 	else
 		ft_printf("(NULL)\n");
 }
