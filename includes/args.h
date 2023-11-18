942fbeacff1d3abfd5bf9860c2ed0f1311821c2c
Drew Pentland <drewtpentland@gmail.com>
2023-11-18 12:16:38 +0100

    Small cleanup with headers in arg_parsing.
    Nothing big

--- includes/args.h
+++ includes/args.h
@@ -6,12 +6,13 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 18:41:01 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/18 11:49:18 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/11/18 12:06:06 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARGS_H
 # define ARGS_H
+
 # include <stdbool.h>
 
 bool	ft_arg_parse(int argc, char **argv);
