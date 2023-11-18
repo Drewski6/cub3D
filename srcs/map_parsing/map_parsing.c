c94226530b82a8338544b1db12dee03c42381877
Drew Pentland <drewtpentland@gmail.com>
2023-11-18 18:20:16 +0100

    Reading in elements well
    Now reading in elements pretty well. Still need to do a little clean up
    though

--- srcs/map_parsing/map_parsing.c
+++ srcs/map_parsing/map_parsing.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 18:36:30 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/18 17:10:39 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/11/18 18:19:13 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@
 #include <string.h>
 #include "ft_printf.h"
 
-bool	ft_save_element(char **dest, char *src)
+bool	ft_save_texture_path(char **dest, char *src)
 {
 	if (*dest)
 		return (ft_putstr_fd("Error: Duplicate element in map.", 2), 1);
@@ -34,19 +34,47 @@ bool	ft_save_element(char **dest, char *src)
 	return (0);
 }
 
+bool	ft_save_rgb_settings(t_rgb *rgb, char *src)
+{
+	char	**tab;
+
+	(void) rgb;
+	ft_striteri(src, ft_newline_to_null);
+	tab = ft_split(src, ',');
+	if (!tab)
+		return (perror("Error: malloc"), 1);
+	if (ft_tablen(tab) != 3)
+		return (ft_table_free(tab),
+			ft_putstr_fd("Error: RGB values incorrect\n", 2), 1);
+	ft_print_table(tab, 1);
+	if (!ft_tabiter_ver(tab, ft_striter_ver, ft_isdigit))
+		return (ft_table_free(tab),
+			ft_putstr_fd("Error: RGB values incorrect\n", 2), 1);
+	rgb->red = ft_atoi(tab[0]);
+	rgb->green = ft_atoi(tab[1]);
+	rgb->blue = ft_atoi(tab[2]);
+	return (ft_table_free(tab), 0);
+}
+
 bool	ft_select_element(t_map_data *map_data, char **tab)
 {
 	if (!ft_strncmp(tab[0], "NO", 3))
-		if (ft_save_element(&map_data->no, tab[1]))
+		if (ft_save_texture_path(&map_data->no, tab[1]))
 			return (1);
 	if (!ft_strncmp(tab[0], "SO", 3))
-		if (ft_save_element(&map_data->so, tab[1]))
+		if (ft_save_texture_path(&map_data->so, tab[1]))
 			return (1);
 	if (!ft_strncmp(tab[0], "EA", 3))
-		if (ft_save_element(&map_data->ea, tab[1]))
+		if (ft_save_texture_path(&map_data->ea, tab[1]))
 			return (1);
 	if (!ft_strncmp(tab[0], "WE", 3))
-		if (ft_save_element(&map_data->we, tab[1]))
+		if (ft_save_texture_path(&map_data->we, tab[1]))
+			return (1);
+	if (!ft_strncmp(tab[0], "F", 2))
+		if (ft_save_rgb_settings(&map_data->f, tab[1]))
+			return (1);
+	if (!ft_strncmp(tab[0], "C", 2))
+		if (ft_save_rgb_settings(&map_data->c, tab[1]))
 			return (1);
 	return (0);
 }
@@ -63,6 +91,8 @@ bool	ft_read_in_elements(t_map_data *map_data, int fd, char **line)
 	{
 		if (**line != '\n')
 		{
+			// if (*line contains only "01NSEW ")
+			// 	were at the map and need to return early.
 			tab = ft_split(*line, ' ');
 			if (!tab)
 				return (perror("Error: malloc"), 1);
