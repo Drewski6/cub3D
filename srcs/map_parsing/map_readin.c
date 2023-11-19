76e05f69b1f4d6ff5ebe8dac890699a575021a9b
Drew Pentland <drewtpentland@gmail.com>
2023-11-19 21:36:14 +0100

    Start map read in
    Reading in map with linked list. But also updating my libft branch

--- srcs/map_parsing/map_readin.c
+++ srcs/map_parsing/map_readin.c
@@ -6,19 +6,31 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 16:59:08 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/18 23:52:20 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/11/19 21:24:23 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 #include "ft_printf.h"
 #include <stdbool.h>
+#include <stdlib.h>
+#include "gnl.h"
+#include "libft.h"
+#include <stdio.h>
+
 
 bool	ft_read_in_map(t_map_data *map_data, int fd, char **line)
 {
-	(void) map_data;
-	(void) fd;
-	(void) line;
-	ft_printf("map line: %s\n", *line);
+	t_list	*current;
+
+	map_data->map = ft_lstnew((void *)*line);
+	while (*line)
+	{
+		*line = get_next_line(fd);
+		current = ft_lstnew(*line);
+		if (!current)
+			return (perror("Error: malloc"), 1);
+		ft_lstadd_back(&map_data->map, current);
+	}
 	return (0);
 }
