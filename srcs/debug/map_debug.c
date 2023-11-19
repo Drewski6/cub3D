6723e5fe38fca4a549cd624ee0fd70bca35035aa
Drew Pentland <drewtpentland@gmail.com>
2023-11-19 22:00:02 +0100

    Added basic reading in the map and clearing

--- srcs/debug/map_debug.c
+++ srcs/debug/map_debug.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 13:41:57 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/18 20:34:16 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/11/19 21:54:32 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,9 +46,9 @@ void	debug_print_map_data(t_map_data *map_data)
 		map_data->f.red, map_data->f.green, map_data->f.blue);
 	ft_printf("c: R(%d), G(%d), B(%d)\n",
 		map_data->c.red, map_data->c.green, map_data->c.blue);
-	ft_printf("map: ");
+	ft_printf("map: \n");
 	if (map_data->map)
-		ft_print_table(map_data->map, 0);
+		ft_lstiter(map_data->map, ft_lst_print_link_nl);
 	else
 		ft_printf("(NULL)\n");
 }
