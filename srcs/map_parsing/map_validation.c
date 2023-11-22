dda2fc752d22b0abe8b04e861a82bb7ccec72cac
Drew Pentland <drewtpentland@gmail.com>
2023-11-22 15:37:36 +0100

    Added some function comments for map_validation.c

--- srcs/map_parsing/map_validation.c
+++ srcs/map_parsing/map_validation.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 14:14:10 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/22 15:27:05 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/11/22 15:37:23 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,24 @@
 #include <stddef.h>
 #include "libft.h"
 #include "ft_printf.h"
+
+/*
+ *	***** ft_map_boarder_check *****
+ *
+ *	DESCRIPTION:
+ *		Check the boarder of the map and make sure everything is a '1' or ' '
+ *		This is because I cant run ft_get_coord_neighbors on the outer boarder
+ *		so to make sure that no spaces touch any unwanted characters I need This
+ *		initial check.
+ *			        
+ *			 111111 
+ *			 1    1 
+ *			 111111 
+ *			        
+ *		Making sure all the '1's above are either '1' or space.
+ *	RETURN:
+ *		Bool function returns 0 on success and 1 on error.
+ */
 
 bool	ft_map_boarder_check(char **map)
 {
@@ -43,6 +61,15 @@ bool	ft_map_boarder_check(char **map)
 	return (0);
 }
 
+/*
+ *	***** ft_map_space_invalid_neighbors *****
+ *
+ *	DESCRIPTION:
+ *		Checks the neighbors of a coordinate for any invalid characters.
+ *	RETURN:
+ *		Bool function returns 0 on success and 1 on error (invalid ch found).
+ */
+
 bool	ft_map_space_invalid_neighbors(char **map, t_coord *coord)
 {
 	ft_get_coord_neighbors(coord, map, '0');
@@ -62,6 +89,18 @@ bool	ft_map_space_invalid_neighbors(char **map, t_coord *coord)
 		return (1);
 	return (0);
 }
+
+/*
+ *	***** ft_map_space_check *****
+ *
+ *	DESCRIPTION:
+ *		Loop over the map and look at every space character and check its
+ *		neighbors to see if they are next to an invalid character.
+ *		Does not check outer space padding layer on map.
+ *		that is why there are y + 1 and y = 1 etc.
+ *	RETURN:
+ *		Bool function returns 0 on success and 1 on error.
+ */
 
 bool	ft_map_space_check(char **map)
 {
@@ -89,6 +128,16 @@ bool	ft_map_space_check(char **map)
 	}
 	return (0);
 }
+
+/*
+ *	***** ft_map_at_least_one_start *****
+ *
+ *	DESCRIPTION:
+ *		Loops over the map and checks to make sure there is at least one
+ *		N, S, E, or W character.
+ *	RETURN:
+ *		Bool function returns 0 on success and 1 on error.
+ */
 
 bool	ft_map_at_least_one_start(char **map)
 {
