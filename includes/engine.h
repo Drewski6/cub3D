60d98e8e470eae77887bae987bf760bf205b7175
Drew Pentland <drewtpentland@gmail.com>
2023-11-23 11:19:49 +0100

    headers and mlx init
    Changed some comments in the headers so that things are uniform.
    also initialzed the mlx pointers (img, mlx, win) in ft_engine_init and
    free the same pointers with ft_free_engine.

--- includes/engine.h
+++ includes/engine.h
@@ -6,13 +6,35 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 10:24:47 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/11/23 10:27:25 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/11/23 11:17:04 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENGINE_H
 # define ENGINE_H
 
-void	ft_engine_init(void);
+//***** includes *****//
+
+# include <stdbool.h>
+
+//***** defines *****//
+
+# define WIN_X 500
+# define WIN_Y 500
+# define WIN_NAME "dpentlan - cub3D"
+
+//***** typesdefs/structs *****//
+
+typedef struct s_engine
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
+}				t_engine;
+
+//***** function declarations *****//
+
+void	ft_free_engine(t_engine *engine);
+bool	ft_engine_init(t_engine *engine);
 
 #endif
