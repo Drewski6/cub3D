301966ad3af88a53dca90d5b840bd8e4c32b8d29
Drew Pentland <drewtpentland@gmail.com>
2023-12-02 09:20:16 +0100

    More improvements on texture loading
    Removed error print statements.
    Reduced number of arguments for ft_xpm_image_init by intializing the ID
    in ft_add_xpm_image.
    Commented out mlx_get_data_addr since I dont think it's necessary for
    how I'm going to use the xpm images.

--- srcs/engine/image_utils/textures.c
+++ srcs/engine/image_utils/textures.c
@@ -6,7 +6,7 @@
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 07:50:07 by dpentlan          #+#    #+#             */
-/*   Updated: 2023/12/02 09:00:01 by dpentlan         ###   ########.fr       */
+/*   Updated: 2023/12/02 09:16:04 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ *	***** ft_xpm_image_init *****
+ *
+ *	DESCRIPTION:
+ *		Initializes the image struct with xpm file.
+ *	RETURN:
+ *		Bool function returns 0 on success and 1 on error.
+ */
+
 bool	ft_xpm_image_init(t_image *image,
-				void *mlx_ptr, t_image_id ID, t_point size, char *filename)
+				void *mlx_ptr, t_point size, char *filename)
 {
 	image->mlx_ptr = mlx_ptr;
-	image->id = ID;
 	image->size = size;
 	image->img_ptr = mlx_xpm_file_to_image(mlx_ptr, filename, &size.x, &size.y);
 	if (!image->img_ptr)
-		return (ft_putstr_fd("failed mlx_xpm\n", 2), 1);
+		return (1);
 	image->bits_per_pixel = 0;
 	image->size_line = 0;
 	image->endian = 0;
 	image->img_buf = NULL;
-	image->img_buf = mlx_get_data_addr(
-			image->img_ptr,
-			&image->bits_per_pixel,
-			&image->size_line,
-			&image->endian);
-	if (!image->img_buf)
-		return (1);
+	// image->img_buf = mlx_get_data_addr(
+	// 		image->img_ptr,
+	// 		&image->bits_per_pixel,
+	// 		&image->size_line,
+	// 		&image->endian);
+	// if (!image->img_buf)
+	// 	return (1);
 	return (0);
 }
+
+/*
+ *	***** ft_add_xpm_image *****
+ *
+ *	DESCRIPTION:
+ *		Mallocs a new image, initializes it, and adds it to the image list.
+ *	RETURN:
+ *		Bool function returns 0 on success and 1 on error.
+ */
 
 bool	ft_add_xpm_image(t_engine *engine, char *filename,
 					t_image_id ID, t_point size)
@@ -50,7 +67,8 @@ bool	ft_add_xpm_image(t_engine *engine, char *filename,
 	new_image = (t_image *)ft_calloc(1, sizeof(t_image));
 	if (!new_image)
 		return (perror("malloc"), 1);
-	if (ft_xpm_image_init(new_image, engine->mlx_ptr, ID, size, filename))
+	new_image->id = ID;
+	if (ft_xpm_image_init(new_image, engine->mlx_ptr, size, filename))
 		return (free(new_image), 1);
 	new_link = ft_lstnew(new_image);
 	if (!new_link)
@@ -58,6 +76,15 @@ bool	ft_add_xpm_image(t_engine *engine, char *filename,
 	ft_lstadd_back(&engine->lst_images, new_link);
 	return (0);
 }
+
+/*
+ *	***** ft_load_textures *****
+ *
+ *	DESCRIPTION:
+ *		Loads each of the 4 xpm textures indicated in the map file.
+ *	RETURN:
+ *		Bool function returns 0 on success and 1 on error.
+ */
 
 bool	ft_load_textures(t_engine *engine, t_map_data *map_data)
 {
@@ -69,19 +96,14 @@ bool	ft_load_textures(t_engine *engine, t_map_data *map_data)
 	if (ft_add_xpm_image(engine, map_data->no, NT,
 			(t_point){width, height}))
 		return (ft_putstr_fd("Error\nCould not open north texture.\n", 2), 1);
-	printf("North Texture: %s loaded!\n", map_data->no);
 	if (ft_add_xpm_image(engine, map_data->so, ST,
 			(t_point){width, height}))
 		return (ft_putstr_fd("Error\nCould not open south texture.\n", 2), 1);
-	printf("South Texture: %s loaded!\n", map_data->so);
 	if (ft_add_xpm_image(engine, map_data->ea, ET,
 			(t_point){width, height}))
 		return (ft_putstr_fd("Error\nCould not open east texture.\n", 2), 1);
-	printf("East Texture: %s loaded!\n", map_data->ea);
 	if (ft_add_xpm_image(engine, map_data->we, WT,
 			(t_point){width, height}))
 		return (ft_putstr_fd("Error\nCould not open west texture.\n", 2), 1);
-	printf("West Texture: %s loaded!\n", map_data->we);
 	return (0);
 }
-
